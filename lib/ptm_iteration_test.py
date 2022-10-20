b3d98d4252c16e7dbd8e709f32257c07c7c9f386
lawashburn <67647952+lawashburn@users.noreply.github.com>
2022-10-20 15:34:03 -0500

    Updated code, bug fix, increased functionality

--- lib/ptm_iteration_test.py
+++ lib/ptm_iteration_test.py
@@ -10,27 +10,54 @@ import pandas as pd
 import time
 start = time.time()
 #sequence = ['ELMAPEN']
-sequence = ['ELEEEEEMAPEN']
+sequence = ['ELEEEEYEMQAPEN','GKEEQ']
 amidation = True
+oxidation_M_status = True
+pyroglu_E_status = True
+pyroglu_Q_status = True
+sulfo_Y_status = True
+phospho_Y_status = True
 max_modifications = 3
 
-mods = ['E(Pyro-glu','M(Oxidation)']
+mods = []
+mod_dict = {}
 
-mod_dict = {'E':'E(Pyro-glu)',
-            'M':'M(Oxidation)'}
+if oxidation_M_status == True:
+    mods.append('M(Oxidation)')
+    mod_dict['M'] = 'M(Oxidation)'
+
+if pyroglu_E_status == True:
+    mods.append('E(Pyro-glu)')
+    mod_dict['E'] = 'E(Pyro-glu)'
+
+if pyroglu_Q_status == True:
+    mods.append('Q(Pyro-glu)')
+    mod_dict['Q'] = 'Q(Pyro-glu)'
+
+if sulfo_Y_status == True:
+    mods.append('Y(Sulfo)')
+    mod_dict['Y'] = 'Y(Sulfo)'
+
+if phospho_Y_status == True:
+    mods.append('Y(Phospho)')
+    mod_dict['Y'] = 'Y(Phospho)'
+
+else:
+    pass
 
 modded_aas = []
 
 for a in mods:
     modded_aas.append(a[0])
 
-aas_present = []
-aas_present_index = []
 
-aas_absent = []
-aas_absent_index = []
-
+final_seq_list = []
 for b in sequence:
+    aas_present = []
+    aas_present_index = []
+    aas_absent = []
+    aas_absent_index = []
+    
     seq_len = len(b)
     for c in range(0,seq_len):
         aa_ID = b[c]
@@ -43,85 +70,82 @@ for b in sequence:
             aas_absent.append(aa_ID)
             aas_absent_index.append(str(c))
 
-number_mods = len(aas_present)
-
-seq_log = pd.DataFrame()
-seq_log['Original Residue'] = aas_present
-seq_log['Index'] = aas_present_index
-seq_log['Res+Index'] = seq_log[['Original Residue', 'Index']].apply(lambda x: ''.join(x), axis=1)
-seq_w_ind = seq_log['Res+Index'].values.tolist()
-
-seq_absent_log = pd.DataFrame()
-seq_absent_log['Original Residue'] = aas_absent
-seq_absent_log['Index'] = aas_absent_index
-
-
-
-for d in range(0,number_mods):
-    seq_w_ind.append('X'+(str(d)))
-
-aa_combination_no_dups = []
-
-if number_mods > max_modifications:
-    aa_combinations = permutations(seq_w_ind,max_modifications)
-    aa_combo_list = list(aa_combinations)
-    for a in aa_combo_list:
-            if a not in aa_combination_no_dups:
-                aa_combination_no_dups.append(a)
-else:
-    aa_combinations = permutations(seq_w_ind,number_mods)
-    aa_combo_list = list(aa_combinations)
-    for a in aa_combo_list:
-            if a not in aa_combination_no_dups:
-                aa_combination_no_dups.append(a)
-
-restyle_aa_list = []
-restyle_aa_ind_list = []
-for z in aa_combination_no_dups:
-    restyle_aa = []
-    restyle_aa_ind = []
-    for y in z:
-        restyle_aa.append(y[0])
-        restyle_aa_ind.append(y[1:])
-    restyle_aa_list.append(restyle_aa)
-    restyle_aa_ind_list.append(restyle_aa_ind)
-
-unmodded_seq = pd.DataFrame()
-unmodded_seq['Original Residue'] = aas_absent
-unmodded_seq['Index'] = aas_absent_index
-
-all_modded_seqs = []
-for k in range(0,len(restyle_aa_list)):
-    new_seq_log = pd.DataFrame()
-    new_seq_log['New Residue'] = restyle_aa_list[k]
-    new_seq_log['Index'] = restyle_aa_ind_list[k]
-    new_seq_log_filtered = new_seq_log[new_seq_log['New Residue'] != 'X']
-    ptm_applied_seq_log = new_seq_log_filtered.replace({'New Residue':mod_dict})
+    number_mods = len(aas_present)
     
-    merge_seq_log = unmodded_seq.merge(ptm_applied_seq_log, on='Index', how='left')
-    ptm_applied_seq_log = merge_seq_log.sort_values(by='Index')
-    ptm_applied_seq_log['New Residue'] = ptm_applied_seq_log['New Residue'].replace('', pd.NA).fillna(ptm_applied_seq_log['Original Residue'])
+    seq_log = pd.DataFrame()
+    seq_log['Original Residue'] = aas_present
+    seq_log['Index'] = aas_present_index
+    seq_log['Res+Index'] = seq_log[['Original Residue', 'Index']].apply(lambda x: ''.join(x), axis=1)
+    seq_w_ind = seq_log['Res+Index'].values.tolist()
+    
+    seq_absent_log = pd.DataFrame()
+    seq_absent_log['Original Residue'] = aas_absent
+    seq_absent_log['Index'] = aas_absent_index
 
-    modified_seq = ptm_applied_seq_log['New Residue'].values.tolist()
-    modified_seq_format = "".join([str(item) for item in modified_seq])
-    all_modded_seqs.append(modified_seq_format)
+    for d in range(0,number_mods):
+        seq_w_ind.append('X'+(str(d)))
+    
+    aa_combination_no_dups = []
+    
+    if number_mods > max_modifications:
+        aa_combinations = permutations(seq_w_ind,max_modifications)
+        aa_combo_list = list(aa_combinations)
+        for a in aa_combo_list:
+                if a not in aa_combination_no_dups:
+                    aa_combination_no_dups.append(a)
+    else:
+        aa_combinations = permutations(seq_w_ind,number_mods)
+        aa_combo_list = list(aa_combinations)
+        for a in aa_combo_list:
+                if a not in aa_combination_no_dups:
+                    aa_combination_no_dups.append(a)
+    
+    restyle_aa_list = []
+    restyle_aa_ind_list = []
+    for z in aa_combination_no_dups:
+        restyle_aa = []
+        restyle_aa_ind = []
+        for y in z:
+            restyle_aa.append(y[0])
+            restyle_aa_ind.append(y[1:])
+        restyle_aa_list.append(restyle_aa)
+        restyle_aa_ind_list.append(restyle_aa_ind)
+    
+    unmodded_seq = pd.DataFrame()
+    unmodded_seq['Original Residue'] = aas_absent
+    unmodded_seq['Index'] = aas_absent_index
+    
+    all_modded_seqs = []
+    for k in range(0,len(restyle_aa_list)):
+        new_seq_log = pd.DataFrame()
+        new_seq_log['New Residue'] = restyle_aa_list[k]
+        new_seq_log['Index'] = restyle_aa_ind_list[k]
+        new_seq_log_filtered = new_seq_log[new_seq_log['New Residue'] != 'X']
+        ptm_applied_seq_log = new_seq_log_filtered.replace({'New Residue':mod_dict})
+        
+        merge_seq_log = unmodded_seq.merge(ptm_applied_seq_log, on='Index', how='left')
+        ptm_applied_seq_log = merge_seq_log.sort_values(by='Index')
+        ptm_applied_seq_log['New Residue'] = ptm_applied_seq_log['New Residue'].replace('', pd.NA).fillna(ptm_applied_seq_log['Original Residue'])
+    
+        modified_seq = ptm_applied_seq_log['New Residue'].values.tolist()
+        modified_seq_format = "".join([str(item) for item in modified_seq])
+        all_modded_seqs.append(modified_seq_format)
+    
+    all_modded_seqs_nodups = []
+    for l in all_modded_seqs:
+        if l not in all_modded_seqs_nodups:
+            all_modded_seqs_nodups.append(l)
+    
+    if amidation == True:
+        for m in all_modded_seqs_nodups:
+            final_seq_list.append(m)
+            amidated_pep = m + '(Amidated)'
+            final_seq_list.append(amidated_pep)
+    else:
+        for m in all_modded_seqs_nodups:
+            final_seq_list.append(m)
 
-all_modded_seqs_nodups = []
-for l in all_modded_seqs:
-    if l not in all_modded_seqs_nodups:
-        all_modded_seqs_nodups.append(l)
-
-terminal_seqs = []
-if amidation == True:
-    for m in all_modded_seqs_nodups:
-        terminal_seqs.append(m)
-        amidated_pep = m + '(Amidated)'
-        terminal_seqs.append(amidated_pep)
-else:
-    for m in all_modded_seqs_nodups:
-        terminal_seqs.append(m)
-
-print(terminal_seqs)
-print(len(terminal_seqs))
+print(final_seq_list)
+print(len(final_seq_list))
 end = time.time()
 print('Analysis complete. Time elapsed:',(end - start),'s')
